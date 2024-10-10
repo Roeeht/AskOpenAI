e6e725fc7d2abc6e1b5f18b8bb7fc62c1078387b
roeeht <roeehabaritamir@gmail.com>
2024-10-10 20:27:08 +0300

    fix config db port typo

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False  # Prevents SQLAlchemy from emitting unnecessary signals
     FLASK_PORT = os.environ.get("FLASK_PORT", 5000)  # Default to 5000 if not set
-    DB_PORT = os.environ.get("FLASK_PORT", 5432) # Default to 5432 if not set
+    DB_PORT = os.environ.get("DB_PORT", 5432) # Default to 5432 if not set
 
